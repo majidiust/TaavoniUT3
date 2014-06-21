a6158143c5ccad5cb2311a85dbb755ac057c29d9
majid.sadeghi.alavijeh@gmail.com <majid.sadeghi.alavijeh@gmail.com>
2014-06-22 01:29:38 +0430

    Add update to list of payment

--- Panels/js/UserDetails.js
+++ Panels/js/UserDetails.js
@@ -276,6 +276,7 @@ CustomBlockingPanel('توجه', 'در حال دریافت اطلاعات پرد�
 						row += "<td>" + newPayment.PaymentDateYear + "/" + newPayment.PaymentDateMonth + "/" + newPayment.PaymentDateDay + "</td>";
 						row += "<td>" + newPayment.PaymentBank + "</td>";
 						row += '<td><button  style="width:100%" class="btn btn-large btn-error" onclick="$(this).parent().parent().remove(); DP(' + "'" + newPayment.PaymentID + "'" +');"> حذف </button></td></tr>';
+						row += '<td><button  style="width:100%" class="btn btn-large btn-info" onclick="alert(' + " ویرایش" + ');"> ویرایش </button></td></tr>';
 						Debug(row);
 						$("#MemberInfoPaymentTable").append(row);
 				}
