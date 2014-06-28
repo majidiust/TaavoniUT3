17cc301d4363aa8e3747bfe99bb6ccf59ef198c2
majid.sadeghi.alavijeh@gmail.com <majid.sadeghi.alavijeh@gmail.com>
2014-06-28 21:33:00 +0430

    Add rankmodel to server side

--- ServerSide/TavooniUT3/TavooniUT3/Controllers/AccountController.cs
+++ ServerSide/TavooniUT3/TavooniUT3/Controllers/AccountController.cs
@@ -1281,13 +1281,29 @@ namespace TavooniUT3.Controllers
              //   if (m_model.aspnet_Users.Count(P => P.UserName.Equals(userName)) > 0)
                 {
                     //      Guid userId = m_model.aspnet_Users.Single(P => P.UserName.Equals(userName)).UserId;
-                    var Result = (from p in m_model.MembersProfiles
+                    var rankList = (from p in m_model.MembersProfiles
                                   select new
                                   {
+                                      FirstName = p.FirstName,
+                                      LastName = p .LastName,
                                       userId = p.MemberID,
                                       NationalityCode = p.InternationalCode,
                                       Point = CalculateUserPoint((Guid)p.MemberID)
-                                  }).OrderBy(P => P.Point).ToList()[0];
+                                  }).OrderBy(P => P.Point).ToList();
+                    List<RankModel> Result = new List<RankModel>();
+                    for (int i = 0 ; i < rankList.Count ; i++)
+                    {
+                        var x = rankList.ElementAt(i);
+                        Result.Add(new RankModel 
+                        { 
+                            FirstName = x.FirstName,
+                            LastName = x.LastName , 
+                            UserId = (Guid)x.userId,
+                            UserName = x.NationalityCode,
+                            Point = x.Point,
+                            Rank = i
+                        });
+                    }
   
                     return Json(new { Status = true, Message = 37, Result }, JsonRequestBehavior.AllowGet);
                 }
