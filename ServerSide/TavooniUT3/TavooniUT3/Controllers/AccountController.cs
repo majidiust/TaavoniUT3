ed218097d8a07ae0c119ece764ea1714b251507c
majid.sadeghi.alavijeh@gmail.com <majid.sadeghi.alavijeh@gmail.com>
2014-06-29 00:45:59 +0430

    Add two stored procedure in order to get rank list and get members rank

--- ServerSide/TavooniUT3/TavooniUT3/Controllers/AccountController.cs
+++ ServerSide/TavooniUT3/TavooniUT3/Controllers/AccountController.cs
@@ -1351,16 +1351,20 @@ namespace TavooniUT3.Controllers
                     Guid userId = m_model.aspnet_Users.Single(P => P.UserName.Equals(userName)).UserId;
                     System.Globalization.PersianCalendar jc = new System.Globalization.PersianCalendar();
                     String tempdate = jc.GetYear((DateTime)DateTime.Now) + ":" + jc.GetMonth((DateTime)DateTime.Now) + ":" + jc.GetDayOfMonth((DateTime)DateTime.Now);
+                    int? rank = 0;
+                    m_model.GetRankForUser(userId, ref rank);
                     var Result = (from p in m_model.MembersProfiles
                                   where p.MemberID.Equals(userId)
                                   select new
                                   {
+                                      UserId= p.MemberID,
                                       NationalityCode = p.InternationalCode,
                                       FirstName = p.FirstName,
                                       LastName = p.LastName,
                                       Date = p.CreateDate != null ? p.CreateDate : tempdate,
                                       IsApproved = p.aspnet_User.aspnet_Membership.IsApproved,
-                                      Point = CalculateUserPoint((Guid)p.MemberID)
+                                      Point = CalculateUserPoint((Guid)p.MemberID),
+                                      Rank = rank
                                   }).ToList()[0];
                     return Json(new { Status = true, Message = 37, Result }, JsonRequestBehavior.AllowGet);
                 }
