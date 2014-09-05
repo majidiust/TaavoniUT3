66f6d74343fd0b6fd8cb83bdcb5885b2f934d9bf
majid.sadeghi.alavijeh@gmail.com <majid.sadeghi.alavijeh@gmail.com>
2014-09-05 16:47:18 +0430

    asd

--- ServerSide/TavooniUT3/TavooniUT3/Controllers/AccountController.cs
+++ ServerSide/TavooniUT3/TavooniUT3/Controllers/AccountController.cs
@@ -1033,7 +1033,7 @@ namespace TavooniUT3.Controllers
                                       BirthDate = p.BirthDate,
                                       Degree = p.DegreeID,
                                       Activity = activity,
-                                      Point = CalculateUserPoint(p.aspnet_User.UserId),
+                                      Point = p.Point,
                                       GuId = p.MemberID
                                   }).ToList()[0];
                     return Json(new { Status = true, Message = 39, Result }, JsonRequestBehavior.AllowGet);
